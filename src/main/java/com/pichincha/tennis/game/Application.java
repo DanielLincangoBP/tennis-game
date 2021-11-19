0d73cb401a9097d1ecde73b622cedfde12acfc5e
gjacomec <gjacomec@pichincha.com>
2021-11-19 10:27:49 -0500

    score

--- src/main/java/com/pichincha/tennis/game/Application.java
+++ src/main/java/com/pichincha/tennis/game/Application.java
@@ -17,11 +17,14 @@ public class Application {
 	private static Integer scorePlayerTwo;
 
 	private static boolean isWinner;
+	private static boolean isDeuce;
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		addPoint();
+		scorePlayerOne = 0;
+		scorePlayerTwo = 0;
+		addPointManual();
 	}
 
 	public void initGame() {
@@ -39,47 +42,81 @@ public class Application {
 		Scanner in = new Scanner(System.in);
 
 		while (!isWinner){
-			/*
-			System.out.print("Player (1 or 2 --> Default 1): ");
-			String playerSelected = in.nextLine();
-			if (playerSelected.compareTo("2")==0) {
-				scorePlayerTwo += 1;
-			} else {
-				scorePlayerOne += 1;
-			}
-			 */
-
 			if (Math.round( Math.random())  == 1) {
 				scorePlayerTwo += 1;
 			} else {
 				scorePlayerOne += 1;
 			}
 			System.out.println(getScore());
-			isWinner = (scorePlayerOne == 3 || scorePlayerTwo == 3);
+			isWinner = validateWinner();
 		}
 
+	}
 
+	public static void addPointManual() {
+		Scanner in = new Scanner(System.in);
+
+		while (!isWinner) {
+			System.out.print("Player (1 or 2 --> Default 1): ");
+			String playerSelected = in.nextLine();
+			if (playerSelected.compareTo("2") == 0) {
+				scorePlayerTwo += 1;
+			} else {
+				scorePlayerOne += 1;
+			}
+			isDeuce = (scorePlayerOne ==scorePlayerTwo) && (scorePlayerTwo >= 3);
+			System.out.println(getScore());
+			isWinner = validateWinner();
+		}
+	}
+
+	private static boolean validateWinner() {
+
+		if (isDeuce) return false;
+		if (scorePlayerTwo <= 3 && scorePlayerOne <= 3) return false;
+		if (scorePlayerTwo == 4 && scorePlayerOne < 3) return true;
+		if (scorePlayerTwo < 3 && scorePlayerOne == 4) return true;
+		return Math.abs (scorePlayerTwo - scorePlayerOne) == 2;
 
 	}
 
 	public static String getScore () {
 
-		 StringBuilder result = new StringBuilder();
-		 result.append("Score Player 1: ");
-		result.append(getResult(scorePlayerOne));
+		StringBuilder result = new StringBuilder();
+		result.append("Score Player 1: ");
+		if (scorePlayerOne<=3) {
+			result.append(getResult(scorePlayerOne));
+		} else if (scorePlayerOne > scorePlayerTwo) {
+			result.append(getResult(5));
+		}else if (scorePlayerOne < scorePlayerTwo) {
+			result.append(getResult(3));
+		} else {
+			result.append(getResult(scorePlayerOne));
+		}
 		result.append(" - Score Player 2: ");
-		result.append(getResult(scorePlayerTwo));
+		if (scorePlayerTwo<=3) {
+			result.append(getResult(scorePlayerTwo));
+		} else if (scorePlayerOne < scorePlayerTwo) {
+			result.append(getResult(5));
+		}else if (scorePlayerOne > scorePlayerTwo) {
+			result.append(getResult(3));
+		} else {
+			result.append(getResult(scorePlayerOne));
+		}
 
 		return result.toString();
 	}
 
 	private static String getResult (Integer points) {
+		String result;
 		Map<Integer, String> score = new HashMap<>();
 		score.put(0, "0");
 		score.put(1, "15");
 		score.put(2, "30");
 		score.put(3, "40");
-		return score.get(points);
+		score.put(4, "WINNER");
+		score.put(5, "ADVANTAGE");
+		return isDeuce ? "DEUCE" :score.get(points);
 
 
 	}
